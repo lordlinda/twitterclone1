3a16b12db5fe8e6ba9d8fa3d16a0821a239daacc
lordlinda <marialindananono@gmail.com>
2020-10-04 23:20:45 +0300

    first commit

---
+++ client/main.js
@@ -0,0 +1,68 @@
+console.log('hello')
+const form = document.querySelector('form')
+const API_URL = window.location.hostname === 'localhost' ? 'http://localhost:5000/mews' : null
+
+const loading = document.querySelector('.loading')
+const mewsList = document.querySelector('.mews')
+loading.style.display = 'none'
+
+listAllMews()
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const formData = new FormData(form)
+    const name = formData.get('name')
+    const content = formData.get('content')
+    const mew = {
+        name,
+        content
+    }
+    loading.style.display = 'block'
+    form.style.display = 'none'
+    fetch(API_URL, {
+            method: 'POST',
+            body: JSON.stringify(mew),
+            headers: {
+                'content-type': 'application/json'
+            }
+        })
+        .then(res => res.json())
+        .then(createdMew => {
+            //console.log(createdMew)
+            listAllMews()
+            form.reset();
+            loading.style.display = 'none'
+            form.style.display = 'block'
+        })
+
+
+})
+
+function listAllMews() {
+    mewsList.innerHTML = ''
+    fetch(API_URL, {
+            method: 'GET',
+            headers: {
+                'content-type': 'application/json'
+            }
+        })
+        .then(res => res.json())
+        .then(mews => {
+            loading.style.display = 'none'
+            form.style.display = 'block'
+            console.log(mews)
+            mews.reverse()
+            mews.forEach(mew => {
+                const header = document.createElement('h3')
+                header.textContent = mew.name
+                const content = document.createElement('p')
+                content.textContent = mew.content
+                const date = document.createElement('p')
+                date.textContent = new Date(mew.created)
+                const div = document.createElement('div')
+                div.appendChild(header)
+                div.appendChild(content)
+                div.appendChild(date)
+                mewsList.appendChild(div)
+            })
+        })
+}
---
+++ server/server.js
@@ -0,0 +1,59 @@
+const express = require('express')
+const cors = require('cors')
+const monk = require('monk')
+const Filter = require('bad-words')
+const rateLimit = require("express-rate-limit");
+
+const filter = new Filter();
+
+const app = express()
+
+const db = monk(process.env.MONGO_URI || "localhost/meower")
+
+const mews = db.get('mews')
+app.use(cors())
+app.use(express.json())
+
+app.get('/mews', (req, res) => {
+    mews.find()
+        .then(mews => {
+            res.json(mews)
+        })
+
+})
+
+function isValidMew(mew) {
+    return mew.name && mew.name.toString().trim() !== '' &&
+        mew.content && mew.content.toString().trim() !== ''
+}
+//this middleware only runs for post method
+app.use(rateLimit({
+    windowMs: 30 * 1000, // 30 seconds
+    max: 1 // limit each IP to 1 requests per windowMs
+}));
+
+app.post('/mews', (req, res) => {
+    //console.log(req.body)
+    if (isValidMew(req.body)) {
+        //insert to db
+        const mew = {
+            name: filter.clean(req.body.name.toString()),
+            content: filter.clean(req.body.content.toString()),
+            created: new Date()
+        }
+        mews.insert(mew)
+            .then(createdMew => {
+                res.json(createdMew)
+            })
+    } else {
+        res.status(422)
+        res.json({
+            message: "name and content are required"
+        })
+    }
+})
+
+
+app.listen(5000, () => {
+    console.log('server is listening on port 5000')
+})
